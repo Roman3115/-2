0d22b430facb61ad83153035a14bbcf16136faae
Roman3115 <168005907+Roman3115@users.noreply.github.com>
2024-05-23 20:32:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,11 +17,11 @@ public class Main {
             e.printStackTrace();
         }
         try {
-            NumberProcessor test1 = new NumberProcessor("file2.txt");
-            int min = test1.findMin();
-            int max = test1.findMax();
-            int sum = test1.sum();
-            double mult = test1.mult();
+            NumberProcessor test2 = new NumberProcessor("file2.txt");
+            int min = test2.findMin();
+            int max = test2.findMax();
+            int sum = test2.sum();
+            double mult = test2.mult();
             System.out.println(min);
             System.out.println(max);
             System.out.println(sum);
@@ -30,11 +30,11 @@ public class Main {
             e.printStackTrace();
         }
         try {
-            NumberProcessor test1 = new NumberProcessor("file3.txt");
-            int min = test1.findMin();
-            int max = test1.findMax();
-            int sum = test1.sum();
-            double mult = test1.mult();
+            NumberProcessor test3 = new NumberProcessor("file3.txt");
+            int min = test3.findMin();
+            int max = test3.findMax();
+            int sum = test3.sum();
+            double mult = test3.mult();
             System.out.println(min);
             System.out.println(max);
             System.out.println(sum);
@@ -43,11 +43,11 @@ public class Main {
             e.printStackTrace();
         }
         try {
-            NumberProcessor test1 = new NumberProcessor("file4.txt");
-            int min = test1.findMin();
-            int max = test1.findMax();
-            int sum = test1.sum();
-            double mult = test1.mult();
+            NumberProcessor test4 = new NumberProcessor("file4.txt");
+            int min = test4.findMin();
+            int max = test4.findMax();
+            int sum = test4.sum();
+            double mult = test4.mult();
             System.out.println(min);
             System.out.println(max);
             System.out.println(sum);
@@ -56,11 +56,11 @@ public class Main {
             e.printStackTrace();
         }
         try {
-            NumberProcessor test1 = new NumberProcessor("file5.txt");
-            int min = test1.findMin();
-            int max = test1.findMax();
-            int sum = test1.sum();
-            double mult = test1.mult();
+            NumberProcessor test5 = new NumberProcessor("file5.txt");
+            int min = test5.findMin();
+            int max = test5.findMax();
+            int sum = test5.sum();
+            double mult = test5.mult();
             System.out.println(min);
             System.out.println(max);
             System.out.println(sum);
